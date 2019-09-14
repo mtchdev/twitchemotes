9ee36bd609b2505c87ea83b84475b9d021815bbb
whenofficial <fsmitchm@gmail.com>
2019-09-14 08:55:21 +0800

    some more processing with lists

--- process.py
+++ process.py
@@ -1,9 +1,32 @@
 import pickle
 from twitchemotes import Emote
+import time
+import sys
 
-with open("usage.obj", "rb") as f:
-    x = pickle.load(f)
-    for i in x:
-        #print(i["emote"].name)
-        print(f"{i['time']} for {i['emote'].name}")
+SECONDS_SPLIT = 5
+
+def process_data():
+    try:
+        with open("usage.obj", "rb") as f:
+            x = pickle.load(f)
+            last = x[0]["time"]
+            batch = [[]]
+            iteration = 0
+            for i in x:
+                time = i["time"]
+                seconds = time-last
+                if seconds < SECONDS_SPLIT:
+                    batch[iteration].append(i["emote"].name)
+                else:
+                    last = time
+                    iteration += 1
+                    batch.append([])
+
+            print(batch)
+    except IOError:
+        print("Couldn't find usage.obj, have you run twitchemotes.py yet?")
+        sys.exit()
+
+if __name__ == "__main__":
+    process_data()
 
